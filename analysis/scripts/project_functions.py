bfc41c879d85b6be3dea42b907c573551d132072
josieleung <71532927+josieleung@users.noreply.github.com>
2020-11-18 17:25:07 -0800

    Update project_functions.py

--- analysis/scripts/project_functions.py
+++ analysis/scripts/project_functions.py
@@ -1,8 +1,3 @@
-import pandas as pd
-
 def load_and_process(directory):
-    
-    df1 = (pd.read_csv(directory)
-          .dropna()
-          )
+    df1 = (pd.read_csv(directory).drop(columns = ["price", "weekly_price"]))
     return df1
